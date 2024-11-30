3a30f5dd788f98d79038175b554d0b7340c88a94
Fanitha G <fnitag@gmail.com>
2024-11-30 13:13:36 -0600

    1st file

---
+++ test1.py
@@ -0,0 +1,38 @@
+print('hello world')
+A=10
+B='edurekha'
+print(A,B)
+x,y,z=10,20,30
+print(x)
+print(y,z)
+a=50
+b=40
+print(a+b)
+print(a%b)
+
+D=[1,2,3.15,'edurekha']
+print(D[2])
+x=10
+Y=12
+if(x<y):
+    print('x is less than y')
+elif(x>y):
+    print('x is less than y')
+else:
+    print(' x and y are equal')
+
+print('testing count')
+count=0
+while(count<5):
+    print(count)
+    count+=1
+print("good bye")
+
+count=1
+for i in range(10):
+    print(str(i)*i)
+
+A=10
+B=10.65
+C=10+6j
+print(A,B,C)
